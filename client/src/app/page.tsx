f437c03c874c6baa26e964788a15dc3db5f2bedd
Hardik Gujrathi <gujarathihardik29@gmail.com>
2025-11-14 21:52:45 +0530

    Initialse the application

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <Button>Hello</Button>
+    </div>
+  );
+}
